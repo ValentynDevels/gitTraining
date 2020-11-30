5531769c28df331bf90fcb910fd0d2cf4e7ace49
Zlatex <maximzlatogorsky@gmail.com>
2020-11-30 13:16:09 +0200

    add sass conf

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,22 @@ const HtmlWebpackPlugin = require('html-webpack-plugin'); // Require  html-webpa
 module.exports = {
   entry: __dirname + "/src/index.js", // webpack entry point. Module to start building dependency graph
   output: {
-    path: __dirname + '/html/js', // Folder to store generated bundle
+    path: __dirname + '/dist', // Folder to store generated bundle
     filename: 'bundle.js',  // Name of generated bundle after build
     publicPath: '/' // public URL of the output directory when referenced in a browser
   },
   module: {  // where we defined file patterns and their loaders
       rules: [ 
+        {
+            test: /\.(sass|scss)$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "sass-loader" // compiles Sass to CSS
+            }]
+          }
       ]
   },
   plugins: [  // Array of plugins to apply to build chunk
